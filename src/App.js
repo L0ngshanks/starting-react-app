9df18b7b4418a28871be58b9e53e759201d63709
LeePJrAAC <lee.paulison@aac-usa.com>
2023-05-29 11:07:45 -0400

    Chapter 10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import styled from "@emotion/styled";
+
+import PokemonInfo from "./components/PokemonInfo";
+import PokemonFilter from "./components/PokemonFilter";
+import PokemonTable from "./components/PokemonTable";
+
+import PokemonContext from "./PokemonContext";
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top" 1rem;
+`;
+
+const Title = styled.h1`
+	text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+	display: grid;
+	grid-template-columns: 80% 20%;
+	gid-column-gap: 1rem;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [filter, setFilter] = React.useState("");
+	const [pokemon, setPokemon] = React.useState([]);
+	const [selectedItem, setSelectedItemSet] = React.useState("");
+
+	React.useEffect(() => {
+		fetch("http://localhost:3000/Pokemon.json")
+			.then((resp) => resp.json())
+			.then((data) => setPokemon(data));
+	}, []);
+
+	return (
+		<PokemonContext.Provider value={{ filter, setFilter, pokemon, setPokemon, selectedItem, setSelectedItemSet }}>
+			<Container>
+				<Title>Pokemon Search</Title>
+				<TwoColumnLayout>
+					<div>
+						<PokemonFilter />
+						<PokemonTable />
+					</div>
+					<div>
+						<PokemonInfo />
+					</div>
+				</TwoColumnLayout>
+			</Container>
+		</PokemonContext.Provider>
+	);
 }
 
 export default App;
